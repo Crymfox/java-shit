8daaeaf4519d11ff8bab6dd3c13eb74fd9be528a
crymfox <sameur.bh@gmail.com>
2022-11-15 17:58:34 -0800

    bullshit

--- tp1/src/heritfood/Apple.java
+++ tp1/src/heritfood/Apple.java
@@ -5,6 +5,6 @@ import food.Fruit;
 public class Apple extends Fruit {
     // declaration d'une methode caracteristique de la classe Apple
     public void caracteristique() {
-        System.out.println("Je suis une pomme");
+        System.out.println("Apple...");
     }
 }
